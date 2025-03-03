8e683d4c405ed6019a48785012de1a2ed80dd90e
Dev Jyoti Ghosh Arnab <devjyotighosharnab@gmail.com>
2025-03-03 00:15:35 -0500

    Initial flashcard website completed without CSS

--- flashcard_which_country/src/App.jsx
+++ flashcard_which_country/src/App.jsx
@@ -1,43 +1,17 @@
 import "./App.css";
 import { useState } from "react";
+import FlashCardList from "./components/flashcardList";
 
 const App = () => {
-
-  const flashcards = [
-    {question: "Press the button for flashcards!", answer: "Start"},
-    {question: "What is sthe capital of Italy?", answer : "Rome"},
-    {question: "What is the capital of United Kingdom?", answer: "London"},
-    {question: "What is the capital of Bangladesh?", answer: "Dhaka"},
-    {question: "What is the capital of United States of America?", answer: "Washington DC"},
-    {question: "Where is Opera House?", answer: "Sydney"},
-    {question: "Which country is called the country of rising sun?", answer: "Japan" }
-  ];
-
-  const [currentCard, setCurrentCard] = useState(0);
-  const [isFlipped, setFlipped] = useState(false);
-
-  const flipCard = () => setFlipped(!isFlipped);
-
-  const nextCard = () => {
-    setCurrentCard(Math.floor(Math.random() * flashcards.length));
-    setFlipped(false);
-  }
 
   return (
     <div className="App">
 
       <div className="header">
         <h1>Flashcards about various countries</h1>
-        <h2>Total cards: {flashcards.length}</h2>
       </div>
 
-      <div className="card" onClick={flipCard}>
-        <h3>
-          {isFlipped ? flashcards[currentCard].answer : flashcards[currentCard].question}
-        </h3>
-      </div>
-
-      <button onClick={nextCard}>Next Card</button>
+      <FlashCardList />
 
     </div>
   )
---
+++ flashcard_which_country/src/components/flashcard.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import React from "react";
+
+
+const Flashcard = ({card, isFlipped, setFlipped}) => {
+
+
+    return (
+        <div className="card" onClick={() => setFlipped(prev => !prev)}>
+            <h3>
+                {isFlipped ? card.answer : card.question}
+            </h3>
+       </div>
+    )
+}
+
+export default Flashcard;
