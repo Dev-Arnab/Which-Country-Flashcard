42d1a36753a59cfa7ed1da9dd9c56273757808b6
Dev Jyoti Ghosh Arnab <devjyotighosharnab@gmail.com>
2025-03-07 01:01:00 -0500

    Added a description in the homepage

--- flashcard_which_country/src/App.jsx
+++ flashcard_which_country/src/App.jsx
@@ -9,6 +9,7 @@ const App = () => {
 
       <div className="header">
         <h1>Flashcards about various countries</h1>
+        <h3>Check your trivia knowledge about various places on earth!</h3>
       </div>
 
       <FlashCardList />
